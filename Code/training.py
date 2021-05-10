e81db5f478cf40296101bc1f6f8a046fa8845143
dina-adel <s-dina.adel@zewailcity.edu.eg>
2021-05-11 00:42:57 +0200

    Update training.py

--- Code/training.py
+++ Code/training.py
@@ -63,7 +63,7 @@ def train(dataset, job_folder, logger, video_root_path):
         data = np.load(os.path.join(video_root_path, '{0}/training_frames_t0.npy'.format(dataset)))
     else:
         data = np.array(HDF5Matrix(os.path.join(video_root_path, '{0}/{0}_train_t{1}.h5'.format(dataset, time_length)), 'data'))
-
+    
     snapshot = ModelCheckpoint(os.path.join(job_folder,
                'model_snapshot_e{epoch:03d}_{val_loss:.6f}.h5'))
 
