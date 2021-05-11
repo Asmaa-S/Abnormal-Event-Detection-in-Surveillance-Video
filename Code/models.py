be50610c595780f49b4fd193fd945c8b329d9ab2
dina-adel <s-dina.adel@zewailcity.edu.eg>
2021-05-11 02:18:46 +0200

    Update models.py

--- Code/models.py
+++ Code/models.py
@@ -16,9 +16,9 @@ def conv_lstm_ae():
     t = cfg['time_length']
     input_tensor = Input(shape=(t, 227, 227, 1))
     
-    conv1 = Conv3D(filters=128, kernel_size=(11, 11), strides=(4, 4), padding='valid', input_shape=(t, 227, 227, 1)
+    conv1 = Conv3D(filters=128, kernel_size=(11, 11, 1), strides=(4, 4,1), padding='valid', input_shape=(t, 227, 227, 1)
             ,activation='relu')
-    conv2 = Conv3D(filters=64, kernel_size=(5, 5), strides=(2, 2), padding='valid', activation='relu')(conv1)
+    conv2 = Conv3D(filters=64, kernel_size=(5, 5,1), strides=(2, 2,1), padding='valid', activation='relu')(conv1)
     
     
     convlstm1 = ConvLSTM2D(filters=64, kernel_size=(3, 3), strides=1, padding='same', dropout=0.4,
@@ -28,7 +28,7 @@ def conv_lstm_ae():
     convlstm3 = ConvLSTM2D(filters=64, kernel_size=(3, 3), strides=1,
                      return_sequences=True, padding='same', dropout=0.5)(convlstm2)
 
-    conv3 = Conv3DTranspose(filters=128,kernel_size=(5,5),strides=(2,2),padding='valid',activation='relu')(convlstm3)
-    decoded = Conv3DTranspose(filters=1,kernel_size=(11,11),strides=(4,4),padding='valid',activation='relu')(conv3)
+    conv3 = Conv3DTranspose(filters=128,kernel_size=(5,5,1),strides=(2,2,1),padding='valid',activation='relu')(convlstm3)
+    decoded = Conv3DTranspose(filters=1,kernel_size=(11,11,1),strides=(4,4,1),padding='valid',activation='relu')(conv3)
 
     return Model(inputs=input_tensor, outputs=decoded)
