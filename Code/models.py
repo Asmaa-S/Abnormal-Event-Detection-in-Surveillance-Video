191320f66ed6b71680b6b52c2ad4400132e20bc8
dina-adel <s-dina.adel@zewailcity.edu.eg>
2021-05-11 02:00:43 +0200

    Update models.py

--- Code/models.py
+++ Code/models.py
@@ -6,22 +6,22 @@ def conv_lstm_ae():
     from keras.layers.normalization import BatchNormalization
     from keras.layers.wrappers import TimeDistributed
     from keras.layers.core import Activation
-    from keras.layers import Input, Flatten, LayerNormalization
+    from keras.layers import Input, Flatten
     import yaml
 
     with open('config.yml', 'r') as ymlfile:
         cfg = yaml.load(ymlfile)
 
     t = cfg['time_length']
-    input_tensor = Input(shape=(None,t, 227, 227, 1))
+    input_tensor = Input(shape=(t, 227, 227, 1))
     conv1 = TimeDistributed(Conv2D(128, kernel_size=(11, 11), padding='same', strides=(4, 4), name='conv1'),
-                            input_shape=(None,t, 227, 227, 1))(input_tensor)
+                            input_shape=(t, 227, 227, 1))(input_tensor)
 
-    conv1 = TimeDistributed(LayerNormalization())(conv1)
+    conv1 = TimeDistributed(BatchNormalization())(conv1)
     conv1 = TimeDistributed(Activation('relu'))(conv1)
 
     conv2 = TimeDistributed(Conv2D(64, kernel_size=(5, 5), padding='same', strides=(2, 2), name='conv2'))(conv1)
-    conv2 = TimeDistributed(LayerNormalization())(conv2)
+    conv2 = TimeDistributed(BatchNormalization())(conv2)
     conv2 = TimeDistributed(Activation('relu'))(conv2)
 
     convlstm1 = ConvLSTM2D(64, kernel_size=(3, 3), padding='same', return_sequences=True, name='convlstm1')(conv2)
@@ -29,7 +29,7 @@ def conv_lstm_ae():
     convlstm3 = ConvLSTM2D(64, kernel_size=(3, 3), padding='same', return_sequences=True, name='convlstm3')(convlstm2)
 
     deconv1 = TimeDistributed(Conv2DTranspose(128, kernel_size=(5, 5), padding='same', strides=(2, 2), name='deconv1'))(convlstm3)
-    deconv1 = TimeDistributed(LayerNormalization())(deconv1)
+    deconv1 = TimeDistributed(BatchNormalization())(deconv1)
     deconv1 = TimeDistributed(Activation('relu'))(deconv1)
 
     decoded = TimeDistributed(Conv2DTranspose(1, kernel_size=(11, 11), padding='same', strides=(4, 4), name='deconv2'))(
